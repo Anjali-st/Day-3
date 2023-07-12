39444d4e4e9fac245c75e82bdd62f49185f4be71
Anjali-st <anjali90908080@gmail.com>
2023-07-13 00:23:39 +0530

    my First commit

---
+++ Exercise-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username;
+            Console.WriteLine("enter the username");
+            username = Console.ReadLine();
+            string Password;
+            Console.WriteLine("enter the password");
+            Password = Console.ReadLine();
+            if (username == "admin1256" && Password == "admin@1256@1256")
+            {
+                Console.WriteLine("SIGN SUCCESS");
+            }
+            else if (username == "" && Password == "")
+            {
+                Console.WriteLine("please provide username and password");
+            }
+            else
+            {
+                Console.WriteLine("invalid user name and password");
+            }
+            Console.ReadKey();
+        }
+    }
+    }
+
---
+++ Exercise-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myNum = 12;
+            var myString = "Sam";
+            var status = true;
+            var grade = 'A';
+            Console.WriteLine("Value Stored in myNum is: "+myNum +"\n Data Type : "+myNum.GetType());
+            Console.WriteLine("Value Stored in myNum is: " + myString + "\n Data Type : " + myString.GetType());
+            Console.WriteLine("Value Stored in myNum is: " + status + "\n Data Type : " + status.GetType());
+            Console.WriteLine("Value Stored in myNum is: " + grade + "\n Data Type : " + grade.GetType());
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
